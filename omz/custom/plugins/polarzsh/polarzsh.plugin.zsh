df591040c3dc5e4f2e5c82a9f542431d57f38ffe
eskopp <info@erik-skopp.de>
2026-03-02 22:34:59 +0100

    fix missmatch

--- omz/custom/plugins/polarzsh/polarzsh.plugin.zsh
+++ omz/custom/plugins/polarzsh/polarzsh.plugin.zsh
@@ -1,10 +1,19 @@
 # polarzsh plugin
 
 export PATH="$HOME/.local/bin:$PATH"
-
 alias cls="clear"
 
 # Enable zoxide if available (smart cd)
 if command -v zoxide >/dev/null 2>&1; then
   eval "$(zoxide init zsh)"
+fi
+
+# Load autosuggestions (Arch package)
+if [[ -r /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+  source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
+
+# Load syntax highlighting (Arch package) -- should be sourced near the end
+if [[ -r /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 fi
